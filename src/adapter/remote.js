11a4a5829659cac43549f4872f619430e843bf6a
Sven <sven@mutzl.org>
2018-04-26 09:46:15 +0200

    Initial commit

---
+++ src/adapter/remote.js
@@ -0,0 +1,110 @@
+const Request = require("superagent");
+
+function get(url, parameters) {
+    return new Promise((resolve, reject) => {
+        return Request.get(url)
+            .send()
+            .set('accept', 'json')
+            .end((err, response) => {
+                try {
+                    response = JSON.parse(response.text);
+                } catch (e) {}
+                if (err) {
+                    reject(Error(err.message));
+                } else if (response.error != undefined)
+                    reject({
+                        name: response.error.code,
+                        message: response.error.message
+                    });
+                else {
+                    resolve(response.result);
+                }
+            });
+    });
+}
+
+let REMOTE = null;
+
+module.exports = (url) => {
+    if (url)
+        REMOTE = url;
+    else
+        REMOTE = 'https://explorer.mvs.org/api/';
+    return {
+        transaction: {
+            get: getTx,
+            list: listTxs
+        },
+        block: {
+            get: getBlock,
+            list: listBlocks
+        },
+        address:{
+            txs: listAddressTxs,
+            utxo: listAddressUtxo
+        },
+        asset:{
+            get: getAsset,
+            list: listAssets
+        }
+    };
+};
+
+function getTx(hash) {
+    return get(`${REMOTE}tx/${hash}`);
+}
+
+function listTxs(page=0, items_per_page=10) {
+    return get(`${REMOTE}txs?page=${page}`);
+}
+
+function getBlock(hash) {
+    return get(`${REMOTE}block/${hash}`);
+}
+
+function listBlocks(page) {
+    return get(`${REMOTE}blocks/${page}`);
+}
+
+function getAsset(symbol) {
+    return get(`${REMOTE}asset/${symbol}`)
+        .then((result)=>result[0]);
+}
+
+function listAssets() {
+    return get(`${REMOTE}assets`);
+}
+
+function listAddressTxs(address, page=0, items_per_page=10) {
+    return get(`${REMOTE}address/txs/${address}?page=${page}&items_per_page=${items_per_page}`);
+}
+
+function listAddressUtxo(address) {
+    return get(`${REMOTE}address/txs/${address}?items_per_page=0`)
+        .then((result) => {
+            let candidates={};
+            for(let i=result.transactions.length-1; i>=0; i--){
+                //Search received outputs
+                if(result.transactions[i].hash=="b076ba7c6667803f166c3e012bcc480cb5c57238f72ba99c51b13070758ce4ce")
+                    console.log(result.transactions[i])
+                result.transactions[i].outputs.forEach((output)=>{
+                    if(output.address==address){
+                        output.locked_until=(output.locked_height_range)?result.transactions[i].height+output.locked_height_range:0;
+                        delete output['locked_height_range'];
+                        output.hash=result.transactions[i].hash;
+                        candidates[result.transactions[i].hash+'-'+output.index]=output;
+                    }
+                });
+                //Remove spent outputs if matching input is found
+                result.transactions[i].inputs.forEach((input)=>{
+                    if(input.address==address){
+                        if(candidates[input.previous_output.hash+'-'+input.previous_output.index]){
+                            candidates[input.previous_output.hash+'-'+input.previous_output.index];
+                        }
+                        else throw Error('Found input without matching output');
+                    }
+                });
+            }
+            return Object.values(candidates);
+        });
+}
---
+++ test/transactions.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+var chai = require("chai"),
+    chaiAsPromised = require("chai-as-promised"),
+    should = chai.should(),
+    Blockchain = require('../')();
+chai.use(chaiAsPromised);
+
+describe('Transactions', function() {
+
+    it('Get transaction', () => {
+        return Blockchain.transaction.get("2a845dfa63a7c20d40dbc4b15c3e970ef36332b367500fd89307053cb4c1a2c1")
+            .then((tx)=>tx.block)
+            .should.become("b81848ef9ae86e84c3da26564bc6ab3a79efc628239d11471ab5cd25c0684c2d");
+    });
+
+});
