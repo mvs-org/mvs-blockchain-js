626ad7976936d4ae627ff89e3ae6a255e2fde538
Sven <sven@mutzl.org>
2018-05-04 10:52:59 +0200

    Addresses functions

--- src/adapter/remote.js
+++ src/adapter/remote.js
@@ -39,11 +39,15 @@ module.exports = (url) => {
             get: getBlock,
             list: listBlocks
         },
-        address:{
+        address: {
             txs: listAddressTxs,
             utxo: listAddressUtxo
         },
-        asset:{
+        addresses: {
+            txs: listAllAddressesTxs,
+            utxo: listAddressesUtxo
+        },
+        asset: {
             get: getAsset,
             list: listAssets
         }
@@ -54,7 +58,7 @@ function getTx(hash) {
     return get(`${REMOTE}tx/${hash}`);
 }
 
-function listTxs(page=0, items_per_page=10) {
+function listTxs(page = 0, items_per_page = 10) {
     return get(`${REMOTE}txs?page=${page}`);
 }
 
@@ -68,41 +72,54 @@ function listBlocks(page) {
 
 function getAsset(symbol) {
     return get(`${REMOTE}asset/${symbol}`)
-        .then((result)=>result[0]);
+        .then((result) => result[0]);
 }
 
 function listAssets() {
     return get(`${REMOTE}assets`);
 }
 
-function listAddressTxs(address, page=0, items_per_page=10) {
+function listAddressTxs(address, page = 0, items_per_page = 10) {
     return get(`${REMOTE}address/txs/${address}?page=${page}&items_per_page=${items_per_page}`);
 }
 
+function listAllAddressesTxs(addresses) {
+    let url = `${REMOTE}addresses/txs?addresses=`+addresses.join('&addresses=');
+    return get(url);
+}
+
 function listAddressUtxo(address) {
-    return get(`${REMOTE}address/txs/${address}?items_per_page=0`)
-        .then((result) => {
-            let candidates={};
-            for(let i=result.transactions.length-1; i>=0; i--){
-                //Search received outputs
-                result.transactions[i].outputs.forEach((output)=>{
-                    if(output.address==address){
-                        output.locked_until=(output.locked_height_range)?result.transactions[i].height+output.locked_height_range:0;
-                        delete output['locked_height_range'];
-                        output.hash=result.transactions[i].hash;
-                        candidates[result.transactions[i].hash+'-'+output.index]=output;
-                    }
-                });
-                //Remove spent outputs if matching input is found
-                result.transactions[i].inputs.forEach((input)=>{
-                    if(input.address==address){
-                        if(candidates[input.previous_output.hash+'-'+input.previous_output.index]){
-                            delete candidates[input.previous_output.hash+'-'+input.previous_output.index];
-                        }
-                        else throw Error('Found input without matching output');
-                    }
-                });
-            }
-            return Object.values(candidates);
-        });
+    return listAddressTxs(address,0,0)
+        .then((txs) => calculateUtxo(txs, [address]));
+}
+
+function listAddressesUtxo(addresses) {
+    return listAllAddressesTxs(addresses)
+        .then((txs) => calculateUtxo(txs, addresses));
+}
+
+function calculateUtxo(txs, addresses) {
+    return new Promise((resolve) => {
+        let candidates = {};
+        for (let i = txs.transactions.length - 1; i >= 0; i--) {
+            //Search received outputs
+            txs.transactions[i].outputs.forEach((output) => {
+                if (addresses.indexOf(output.address) !== -1) {
+                    output.locked_until = (output.locked_height_range) ? txs.transactions[i].height + output.locked_height_range : 0;
+                    delete output['locked_height_range'];
+                    output.hash = txs.transactions[i].hash;
+                    candidates[txs.transactions[i].hash + '-' + output.index] = output;
+                }
+            });
+            //Remove spent outputs if matching input is found
+            txs.transactions[i].inputs.forEach((input) => {
+                if (addresses.indexOf(input.address) !== -1) {
+                    if (candidates[input.previous_output.hash + '-' + input.previous_output.index]) {
+                        delete candidates[input.previous_output.hash + '-' + input.previous_output.index];
+                    } else throw Error('Found input without matching output');
+                }
+            });
+        }
+        resolve(Object.values(candidates));
+    });
 }
