59b6298183efe73846433326c1a7dbe6174e48db
Sven <sven@mutzl.org>
2018-05-07 12:05:05 +0200

    Examples to deposit and issue

--- example/deposit.js
+++ example/deposit.js
@@ -3,18 +3,17 @@ let blockchain = require('..')({
 });
 let Metaverse = require('metaversejs');
 
-var target = {
-    ETP: 100000
-};
+var quantity = 1000;
+var duration = 25200;
 
-var recipient_address="MDyq6w7RqXPF9F5SSrKpTrharr8wF1D4gX";
-var change_address="MKXYH2MhpvA3GU7kMk8y3SoywGnyHEj5SB";
+var recipient_address = "MDyq6w7RqXPF9F5SSrKpTrharr8wF1D4gX";
+var change_address = "MDc9rsRr5Ukgro4mu89G2Spdts5aACRJdb";
 
 Metaverse.wallet.fromMnemonic("lunar there win define minor shadow damage lounge bitter abstract sail alcohol yellow left lift vapor tourist rent gloom sustain gym dry congress zero")
     .then((wallet)=>{
         blockchain.addresses.utxo(wallet.getAddresses())//Get all utxo
-            .then((utxos) => Metaverse.transaction_builder.findUtxo(utxos, target)) //Collect utxo for given target
-            .then((result) => Metaverse.transaction_builder.send(result.utxo, recipient_address, target, change_address, result.change))
+            .then((utxos) => Metaverse.transaction_builder.findUtxo(utxos, {'ETP': quantity},Metaverse.transaction.DEFAULT_FEE)) //Collect utxo for given target
+            .then((result) => Metaverse.transaction_builder.deposit(result.utxo, recipient_address, quantity, duration, change_address, result.change))
             .then((tx)=>wallet.sign(tx))
             .then((tx)=>tx.encode())
             .then(tx => console.log(tx.toString('hex')));
---
+++ example/issue.js
@@ -0,0 +1,24 @@
+let blockchain = require('..')({
+    url: "https://explorer.mvs.org/api/"
+});
+let Metaverse = require('metaversejs');
+
+const symbol='TEST.TEST',
+      max_supply=10,
+      precision=2,
+      issuer='tester',
+      description='some boring asset';
+
+var recipient_address = "MDyq6w7RqXPF9F5SSrKpTrharr8wF1D4gX";
+var change_address = "MDc9rsRr5Ukgro4mu89G2Spdts5aACRJdb";
+
+Metaverse.wallet.fromMnemonic("lunar there win define minor shadow damage lounge bitter abstract sail alcohol yellow left lift vapor tourist rent gloom sustain gym dry congress zero")
+    .then((wallet)=>{
+        blockchain.addresses.utxo(wallet.getAddresses())//Get all utxo
+            .then((utxos) => Metaverse.transaction_builder.findUtxo(utxos, {},Metaverse.transaction.ASSET_ISSUE_DEFAULT_FEE)) //Collect utxo for given target
+            .then((result) => Metaverse.transaction_builder.issue(result.utxo, recipient_address, symbol, max_supply, precision, issuer, description, change_address, result.change))
+            .then((tx)=>wallet.sign(tx))
+            .then((tx)=>tx.encode())
+            .then(tx => console.log(tx.toString('hex')));
+    })
+    .catch(console.error);
