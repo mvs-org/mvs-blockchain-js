edd1399be3622ed92f327e3eee7f3536cd7be2f6
Sven <sven@mutzl.org>
2018-05-10 11:47:24 +0200

    Grunt config

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec',
+                    captureFile: './dist/result.txt',
+                    quiet: false,
+                    timeout: 10000,
+                    clearRequireCache: false,
+                    noFail: false
+                },
+                src: ['./test/*.js']
+            }
+        },
+        browserify: {
+            all: {
+                options: {
+                    browserifyOptions: {
+                        standalone: 'Blockchain'
+                    }
+                },
+                src: ['./index.js'],
+                dest: './dist/index.js',
+            }
+        },
+        babel: {
+            options: {
+                sourceMap: true,
+                presets: ['env']
+            },
+            dist: {
+                files: {
+                    './dist/index.js': './dist/index.js'
+                }
+            }
+        },
+        uglify: {
+            js: {
+                src: ['./dist/index.js'],
+                dest: './dist/index.min.js'
+            }
+        }
+
+    });
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-babel');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-mocha-test');
+
+    grunt.registerTask('default', ['mochaTest', 'browserify', 'babel', 'uglify']);
+};
