e2d83a2b8fc58983197595008c315531d7cfaeed
Sven <sven@mutzl.org>
2018-05-10 11:22:49 +0200

    Address balances example

---
+++ example/address_balances.js
@@ -0,0 +1,15 @@
+let blockchain = require('..')({
+    url: "https://explorer.mvs.org/api/"
+});
+let Metaverse = require('metaversejs');
+
+Metaverse.wallet.fromMnemonic("lunar there win define minor shadow damage lounge bitter abstract sail alcohol yellow left lift vapor tourist rent gloom sustain gym dry congress zero")
+    .then((wallet) =>
+        blockchain.height()
+        .then((height) => {
+            blockchain.addresses.txs(wallet.getAddresses()) //Get transactions
+                .then((txs) => blockchain.balance.addresses(txs.transactions, wallet.getAddresses(), height)) //
+                .then(console.log);
+        })
+    )
+    .catch(console.error);
