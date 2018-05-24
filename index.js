ef51a0630e853eec46d981415068f2c17c5a2c97
Sven <sven@mutzl.org>
2018-05-24 10:13:49 +0200

    Testnet capabilities

--- index.js
+++ index.js
@@ -10,5 +10,8 @@
 
 module.exports = (options={}) => {
     if (options.source && options.source != 'remote') throw Error(`Unknown source ${options.source}`);
+    if (options.network == 'testnet' && options.url == undefined){
+        options.url='https://explorer-testnet.mvs.org';
+    }
     return require('./src/adapter/remote.js')(options.url);
 };
