a6b51f8f753154853ad4dbf3f898e14d118eac44
Sven <sven@mutzl.org>
2018-05-24 10:17:23 +0200

    Testnet explorer url

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
 module.exports = (options={}) => {
     if (options.source && options.source != 'remote') throw Error(`Unknown source ${options.source}`);
     if (options.network == 'testnet' && options.url == undefined){
-        options.url='https://explorer-testnet.mvs.org';
+        options.url='https://explorer-testnet.mvs.org/api/';
     }
     return require('./src/adapter/remote.js')(options.url);
 };
